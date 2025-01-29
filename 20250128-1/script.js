f1b05fc8055e27a7e78b66472709b03a7daf08fc
satoru <rhodeus208@gmail.com>
2025-01-30 00:13:54 +0900

    課題3がまだ

--- 20250128-1/script.js
+++ 20250128-1/script.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded',()=>{
 
     const decrementCount=(counter)=>{
         let curr = parseInt(counter.textContent.match(numRegex)[0]);
-        curr--;
+        curr=curr-1;
         counter.textContent=`総タスク数： ${curr}`;
     }
 
@@ -45,6 +45,71 @@ document.addEventListener('DOMContentLoaded',()=>{
         taskItem.appendChild(completeBtn);
     }
 
+    const setDeleteFunction=(item,btn,counter)=>{
+        btn.addEventListener('click',()=>{
+            item.remove();
+            decrementCount(counter);
+        })
+    }
+
+    const setDeleteBtn=(taskItem,counter)=>{
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent='削除';
+        setDeleteFunction(taskItem,deleteBtn,counter);
+        taskItem.appendChild(deleteBtn);
+    }
+
+    const translate=(english)=>{
+        switch(english){
+            case 'high':
+                return '高';
+            case 'medium':
+                return '中';
+            default :
+                return '低';
+        }
+    }
+
+    const createTaskName=(input,select)=>{
+        const priority = translate(select);
+        const text=input.value.trim();
+
+        return `${priority}: ${text}`;
+    }
+
+    const toggleDisplay=(items,priority)=>{
+        if(priority.value=='all'){
+            [...items].forEach(element => {
+                element.style.display='block';
+            });
+            return;
+        }
+
+        const className = `${priority}-priority`;
+
+        console.log(className);
+
+        [...items].forEach(element=>{
+            if(element.classList.contains(className)){
+                element.style.display='block';
+                console.log('１つ目');
+            }else{
+                element.style.display='none';
+                console.log('２つ目');
+            }
+        })
+        
+    }
+
+    const addToggleFunction=(selector)=>{
+        selector.addEventListener('change',()=>{
+            const items = document.getElementsByClassName('taskItem');
+            toggleDisplay(items,selector);
+        })
+    }
+
+    addToggleFunction(priorityFilter);
+
     const addTask=(input,select,list,count,error)=>{
         if(!checkInput(input)){
             displayError(error);
@@ -53,17 +118,21 @@ document.addEventListener('DOMContentLoaded',()=>{
 
         const text = input.value.trim();
         const task_name = document.createElement('span');
-        task_name.textContent = text;
         const priority = select.value;
+        task_name.textContent = createTaskName(input,priority);
+        
 
         const taskItem = document.createElement('div');
         taskItem.appendChild(task_name);
         taskItem.className = 'task-item';
         taskItem.classList.add(`${priority}-priority`);
         setCompleteBtn(taskItem);
+        setDeleteBtn(taskItem,taskCount);
         
         list.appendChild(taskItem);
         incrementCount(count);
+
+        input.value='';
 
     }
 
